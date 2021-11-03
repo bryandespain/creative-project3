5e68bc631e82b31808a9b703b3e80c4d53d945a1
bryandespain <bryanjd@byu.edu>
2021-11-02 20:51:18 -0600

    added images

---
+++ src/components/PlayerList.vue
@@ -0,0 +1,91 @@
+<template>
+<div class="wrapper">
+  <div class="players">
+    <div class="player" v-for="player in this.$root.$data.players" :key="player.name">
+      <div class = "name">
+        <h2>{{player.name}} #{{player.number}}</h2>
+      </div>
+      <div class="image">
+        <img src = "/images/damianlillard.jp">
+        <img :src = "'/images/'+player.image">
+      </div>
+    </div>
+  </div>
+</div>
+</template>
+<script>
+  export default {
+    name: 'PlayerList',
+    props: {
+      players: Array
+    }
+  }
+</script>
+<style scoped>
+.wrapper {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.players {
+  margin-top: 20px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+}
+
+.player {
+  margin: 10px;
+  margin-top: 50px;
+  width: 200px;
+}
+
+.player img {
+  border: 2px solid #333;
+  height: 250px;
+  width: 200px;
+  object-fit: cover;
+}
+
+.player .image {
+  display: flex;
+  justify-content: center;
+  margin-bottom: 5px;
+}
+
+.info {
+  background: #F2921D;
+  color: #000;
+  padding: 10px 30px;
+  height: 80px;
+}
+
+.name h1 {
+  font-size: 16px;
+}
+
+.name h2 {
+  font-size: 14px;
+}
+
+.name p {
+  margin: 0px;
+  font-size: 10px;
+}
+
+.price {
+  display: flex;
+}
+
+button {
+  height: 50px;
+  background: #000;
+  color: white;
+  border: none;
+}
+
+.auto {
+  margin-left: auto;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,52 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+<div>
+  <div class="wrapper">
   </div>
+  <PlayerList :players="players" />
+</div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
+
+<script>
+import PlayerList from "../components/PlayerList.vue"
 export default {
-  name: "Home",
+  name: 'Home',
   components: {
-    HelloWorld,
-  },
-};
+    PlayerList
+  }
+}
 </script>
+<style scoped>
+.wrapper {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.search {
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  width: 50%;
+}
+
+form {
+  display: table;
+  width: 100%;
+}
+
+i {
+  display: table-cell;
+  padding-left: 10px;
+  width: 1px;
+}
+
+input {
+  display: table-cell;
+  font-size: 20px;
+  border: none !important;
+  box-shadow: none !important;
+  width: 100%;
+  height: 40px;
+}
+</style>
